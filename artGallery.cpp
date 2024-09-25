5dc9fa04ff061d32e4a0dfe3b4c343f9a282d558
omjadhav1910 <sonujadhav7020@gmail.com>
2024-09-25 09:50:00 +0530

    Encapsulation

--- artGallery.cpp
+++ artGallery.cpp
@@ -3,103 +3,57 @@
 using namespace std;
 
 class ArtPiece {
-    public:
+    private:
         string title;
-        string artist;
-        string medium;
         int yearCreated;
-        static int totalArtPieces;   
-
-        ArtPiece(string t, string a, string m, int y) {
-            this->title = t;
-            this->artist = a;
-            this->medium = m;
-            this->yearCreated = y;
-            totalArtPieces++;   
+    
+    public:
+        ArtPiece(string t, int y) {
+            title = t;
+            yearCreated = y;
         }
 
-        void displayInfo() {
-            cout << "Title: " << this->title << endl;
-            cout << "Artist: " << this->artist << endl;
-            cout << "Medium: " << this->medium << endl;
-            cout << "Year Created: " << this->yearCreated << endl << endl;
-        }
+        string getTitle() const { return title; }
+        int getYearCreated() const { return yearCreated; }
+        void setYearCreated(int y) { yearCreated = y; }
 
-        void updateYear(int newYear) {
-            this->yearCreated = newYear;
-        }
-
-        static void displayTotalArtPieces() {
-            cout << "Total Art Pieces: " << totalArtPieces << endl;   
+        void displayInfo() const {
+            cout << "Title: " << getTitle() << ", Year Created: " << getYearCreated() << endl;
         }
 };
-
-int ArtPiece::totalArtPieces = 0;
 
 class Artist {
-    public:
+    private:
         string name;
-        string style;
-        ArtPiece* artworks[2];
-        static int totalArtists;   
-
-        Artist(string n, string s, ArtPiece* art1, ArtPiece* art2) {
-            this->name = n;
-            this->style = s;
-            this->artworks[0] = art1;
-            this->artworks[1] = art2;
-            totalArtists++;   
+        ArtPiece* artwork;
+    
+    public:
+        Artist(string n, ArtPiece* art) {
+            name = n;
+            artwork = art;
         }
 
-        void displayInfo() {
-            cout << "Artist: " << this->name << endl;
-            cout << "Style: " << this->style << endl;
-            cout << "Artworks:" << endl;
-            for(int i = 0; i < 2; i++) {
-                artworks[i]->displayInfo();  
-            }
+        void displayInfo() const {
+            cout << "Artist: " << name << endl;
+            artwork->displayInfo();
         }
 
-        void updateArt(int index, int newYear) {
-            artworks[index]->updateYear(newYear);  
-        }
-
-        ~Artist() {
-            for(int i = 0; i < 2; i++) {
-                delete artworks[i];
-            }
-        }
-
-        static void displayTotalArtists() {
-            cout << "Total Artists: " << totalArtists << endl;   
+        void updateArtYear(int newYear) {
+            artwork->setYearCreated(newYear);
         }
 };
 
-int Artist::totalArtists = 0;
-
 int main() {
-    ArtPiece* art1 = new ArtPiece("Starry Night", "Vincent van Gogh", "Oil on canvas", 1889);
-    ArtPiece* art2 = new ArtPiece("Sunflowers", "Vincent van Gogh", "Oil on canvas", 1888);
-    ArtPiece* art3 = new ArtPiece("The Persistence of Memory", "Salvador Dali", "Oil on canvas", 1931);
-    ArtPiece* art4 = new ArtPiece("Galatea of the Spheres", "Salvador Dali", "Oil on canvas", 1952);
+    ArtPiece* art1 = new ArtPiece("Starry Night", 1889);
+    Artist* artist1 = new Artist("Vincent van Gogh", art1);
 
-    Artist* artist1 = new Artist("Vincent van Gogh", "Post-Impressionism", art1, art2);
-    Artist* artist2 = new Artist("Salvador Dali", "Surrealism", art3, art4);
+    artist1->displayInfo();
 
-    Artist* artists[2] = {artist1, artist2};
-
-    for(int i = 0; i < 2; i++) {
-        artists[i]->displayInfo();
-    }
-
-    artists[0]->updateArt(0, 2024);
-    artists[0]->displayInfo();
-
-    ArtPiece::displayTotalArtPieces();
-    Artist::displayTotalArtists();
+    artist1->updateArtYear(2024);
+    artist1->displayInfo();
 
     delete artist1;
-    delete artist2;
+    delete art1;
 
     return 0;
 }
